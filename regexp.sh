f93b4c050f0c5eb27d88d803cdd94cff2ba301a5
Nicklas Van Dam <herlambangdicky5@gmail.com>
2016-11-05 16:18:25 +0700

    Final fix for simple regexp script

--- regexp.sh
+++ regexp.sh
@@ -16,37 +16,34 @@ find -name "*.c" | xargs sed -i "s/^__cpuinit //g"
 
 echo "##Done"
 
-echo
-"###Running second regexp init"
+echo "###Running second regexp init"
 
 find arch/ -name "*.h" | xargs sed -i "s/ __cpuinit//g"
 
 echo "##Done"
 
-echo
-"###Running third regexp init"
+echo "###Running third regexp init"
 
 find -name "*.c" | xargs sed -i "s/ __cpuinit$//g"
 
 echo "##Done"
 
-"###Running fourth regexp init"
+echo "###Running fourth regexp init"
 
 find -name "*.c" | xargs sed -i "s/ __cpuinitdata//g"
 
 echo "##Done"
 
-echo
-"###Running fifth regexp init"
+echo "###Running fifth regexp init"
 
 find -name "*.c" | xargs sed -i "s/ __cpuexit / /g"
 
 echo "##Done"
 
-echo
-"###Running sixth regexp init"
+echo "###Running sixth regexp init"
 
 find -name "*.c" | xargs sed -i "s/ __cpuinit / /g"
+
 echo "##Done"
 
 echo "##Regexp process complete"
